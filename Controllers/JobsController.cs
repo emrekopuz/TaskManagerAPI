7d07afc1ccd9add9f0b50f7bcab38f528a5aadf0
emrekopuz <emrekopuz95@gmail.com>
2020-06-07 00:21:24 +0300

    Signed-off-by: emrekopuz <emrekopuz95@gmail.com>

---
+++ Controllers/JobsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagerAPI.Models;
+using TaskManagerAPI.Services;
+
+namespace TaskManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private readonly JobService _jobService;
+
+        public JobsController(JobService jobService)
+        {
+            _jobService = jobService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Job>> Get() =>
+            _jobService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetJob")]
+        public ActionResult<Job> Get(string id)
+        {
+            var job = _jobService.Get(id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return job;
+        }
+
+        [HttpPost]
+        public ActionResult<Job> Create(Job job)
+        {
+            _jobService.Create(job);
+
+            return CreatedAtRoute("GetJob", new { id = job.Id.ToString() }, job);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Job jobIn)
+        {
+            var job = _jobService.Get(id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            _jobService.Update(id, jobIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var job = _jobService.Get(id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            _jobService.Remove(job.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Services/JobService.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagerAPI.Models;
+
+namespace TaskManagerAPI.Services
+{
+    public class JobService
+    {
+        private readonly IMongoCollection<Job> _jobs;
+
+        public JobService(IToDoDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _jobs = database.GetCollection<Job>(settings.JobsCollectionName);
+        }
+
+        public List<Job> Get() =>
+            _jobs.Find(job => true).ToList();
+
+        public Job Get(string id) =>
+            _jobs.Find<Job>(job => job.Id == id).FirstOrDefault();
+
+        public Job Create(Job job)
+        {
+            _jobs.InsertOne(job);
+            return job;
+        }
+
+        public void Update(string id, Job jobIn) =>
+            _jobs.ReplaceOne(job => job.Id == id, jobIn);
+
+        public void Remove(Job jobIn) =>
+            _jobs.DeleteOne(job => job.Id == jobIn.Id);
+
+        public void Remove(string id) =>
+            _jobs.DeleteOne(job => job.Id == id);
+    }
+}
